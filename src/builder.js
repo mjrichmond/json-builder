9b976aa5110429b7442eb0f37a614fb99989a7c8
Mattias Erming <mattias@mattiaserming.com>
2014-07-29 09:31:02 -0700

    Added form rendering

--- src/builder.js
+++ src/builder.js
@@ -1,3 +1,64 @@
 function Builder() {
 	// ..
 }
+
+Builder.prototype.buildForm = function(json, name, html) {
+	if (!json.type) {
+		return;
+	}
+
+	name = name || "";
+	html = html || "";
+
+	switch (json.type) {
+	case "array":
+		var items = json.items;
+		html = this.renderTemplate(
+			"array", {
+				name: name,
+				html: this.buildForm(items, name)
+			}
+		);
+		break;
+		
+	case "object":
+		if (name) {
+			name += ".";
+		}
+		var props = json.properties;
+		for (var i in props) {
+			html += this.buildForm(props[i], (name || "") + i);
+		}
+		html = this.renderTemplate(
+			"object", {
+				name: name,
+				html: html
+			}
+		);
+		break;
+	
+	case "string":
+		html = this.renderTemplate(
+			"string", {
+				name: name
+			}
+		);
+		break;
+	}
+
+	return html;
+};
+
+Builder.prototype.getTemplate = function(name) {
+	return $("#templates")
+		.find("script[data-name='" + name+ "']")
+		.html();
+}
+
+var templates = [];
+Builder.prototype.renderTemplate = function(name, data) {
+	if (!templates[name]) {
+		templates[name] = Handlebars.compile(this.getTemplate(name)); 
+	}
+	return templates[name](data);
+};
